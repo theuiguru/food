8af7711b0e79da443ac83f7c1d016e03413c7a78
Chris Thomas <webmaster102@gmail.com>
2018-07-03 23:56:38 -0400

    added #word to foods[]

--- assets/food.js
+++ assets/food.js
@@ -1,6 +1,8 @@
 var foods = ['burger', 'pizza', 'taco', 'burrito', 'chicken', 'nachos', 'sandwich', 'veggie'],
     b = Math.floor(Math.random() * foods.length),
     foodImg = document.querySelector(".food"),
-    mood = document.getElementById("mood");
+    mood = document.getElementById("mood"),
+    word = document.getElementById("word");
 foodImg.style.backgroundImage = 'url(./assets/food/' + foods[b] + '.jpg)';
+word.innerHTML = foods[b];
 mood.setAttribute('href', 'https://foursquare.com/explore?q=' + foods[b]);
