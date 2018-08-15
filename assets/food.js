51104e64a5bd780462de5ea754951fe075f9d408
Chris Thomas <webmaster102@gmail.com>
2018-08-15 11:13:23 -0400

    remade layout and scripts

--- assets/food.js
+++ assets/food.js
@@ -1,19 +1,13 @@
 var foods = ['burger', 'pizza', 'taco', 'burrito', 'chicken', 'nachos', 'sandwich', 'vegetarian', 'halal'],
-	intros = ['You should get', 'Dude.', 'Definitely time for some', 'Don’t you feel like getting', 'How about', 'Know what would hit the spot?', 'Feels like a day for', 'Why not get some', 'Need somethin healthy. How ’bout', 'Indulge in', 'What about', 'Hmm…', 'Let’s treat ourselves to', 'Aren’t you in the mood for', 'Maybe', 'Been a while since you’ve gotten'],
-    b = Math.floor(Math.random() * foods.length),
+    intros = ['You should get', 'Dude.', 'Definitely time for some', 'Don’t you feel like getting', 'How about', 'Know what would hit the spot?', 'Feels like a day for', 'Why not get some', 'Need somethin healthy. How ’bout', 'Indulge in', 'What about', 'Hmm…', 'Let’s treat ourselves to', 'Aren’t you in the mood for', 'Maybe', 'Been a while since you’ve gotten'],
+    f = Math.floor(Math.random() * foods.length),
     i = Math.floor(Math.random() * intros.length),
-    foodImg = document.querySelector("body"),
-    food = document.getElementById("food"),
-    intro = document.getElementById("intro"),
-    button = document.getElementById("button");
-foodImg.style.backgroundImage = 'url(./assets/food/' + foods[b] + '.jpg)';
-foodImg.style.backgroundPosition = "center top";
-foodImg.style.backgroundSize = "cover";
+    intro = document.querySelector('.intro'),
+    food = document.querySelector('.food'),
+    button = document.querySelector('.btn'),
+    foodBg = document.querySelector("body");
+foodBg.style.backgroundImage = 'url(./assets/food/' + foods[f] + '.jpg)';
 intro.innerHTML = intros[i];
-food.innerHTML = foods[b];
-button.addEventListener('click', function() {
-	location.reload();
-});
-food.addEventListener('click', function() {
-	location.href='https://foursquare.com/explore?q=' + foods[b];
-});
+food.innerHTML = foods[f];
+button.addEventListener('click', function() { location.reload(); });
+food.addEventListener('click', function() { location.href='https://foursquare.com/explore?q=' + foods[f]; });
